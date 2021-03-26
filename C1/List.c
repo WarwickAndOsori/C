c619a6e6b00ca0d3473178aede8a33588c2b9f6d
vixenturefox <20051@gsm.hs.kr>
2021-03-26 09:21:07 +0900

    단순 리스트 선언하는 방법

---
+++ C1/List.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef int element;
+typedef struct ListNode {
+	element data;  // 데이터 필드: element타입으로 데이터 저장
+
+	struct ListNode* link; // 링크 필드 : 현제 구조체를 가르키는 포인터 정의
+}ListNode;
+
+
+int main(void) {
+	ListNode* ph; // 포인터 생성
+	ph = (ListNode*)malloc(sizeof(ListNode)); // 노드 크기만큼 동적 메모리 할당
+	
+}
